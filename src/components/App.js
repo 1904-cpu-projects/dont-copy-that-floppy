abe260ce1157565f6398926f259cb3a17f36f303
Nica Zulueta <nicacassandrazulueta@gmail.com>
2019-08-01 19:20:21 -0700

    added header and some starting components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
+import Header from './Header.js'
+import Home from './Home.js'
+import Products from './Products.js'
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <HashRouter>
+        <h2>Floppy Shoppy</h2>
+        <Route path='/' component={Header} />
+        <Route exact path='/' component={Home} />
+        <Route path='/products' component={Products} />
+      </HashRouter>
+    )
   }
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Products
