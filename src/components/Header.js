5c9b2a6f0ec3de7157d8214fcf1db2340ca76999
Ryan Moore <rymoore411@gmail.com>
2019-08-04 18:36:42 -0700

    can create user

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 
-const Header = ({ products }) => {
-  //console.log(products);
+const Header = ({ products, user }) => {
+  console.log(user);
   return (
     <div>
       <Link to='/'>Home</Link>
       <Link to='/products'>Products ({products.length})</Link>
       <Link to='/cart'>Cart</Link>
       <Link to='/login'>Log In</Link>
-      <Link to='/signup'>Sign Up</Link>
+
+      <Link to='/signup'>{(!user.email) ? "Sign Up" : `Welcome ${user.firstName}`}</Link>
     </div>
   )
 }
 
-const mapStateToProps = ({products})=>{
+const mapStateToProps = ({products, user})=>{
   return {
     products,
+    user,
   }
 }
 
