6afb628a40505201a06a75b6d9a35d6e2b394ceb
Nica Zulueta <nicacassandrazulueta@gmail.com>
2019-08-12 21:05:18 -0700

    clears cart on checkout front end

--- src/components/OrderConfirmation.js
+++ src/components/OrderConfirmation.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deleteCart } from '../store'
 
-const OrderConfirmation = ({ addedProduct }) => {
-  function orderNumber() {
+class OrderConfirmation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.orderNumber = this.orderNumber.bind(this);
+  }
+
+  componentDidMount () {
+    this.props.deleteCart();
+  }
+
+  orderNumber() {
     let now = Date.now().toString()
     now += now + Math.floor(Math.random() * 10)
     return  [now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-')
   }
 
-  return (
+  render () {
+    return (
     <div>
       <h2>Thanks for shopping with us!</h2>
-      <h3>Order Number: {orderNumber()}</h3>
-      <h3>Order Details:</h3>
+      <h3>Order Number: {this.orderNumber()}</h3>
     </div>
-  );
+  )};
 }
+
 
 const mapStateToProps = ({ addedProduct }) => {
   return {
@@ -23,4 +34,8 @@ const mapStateToProps = ({ addedProduct }) => {
   };
 };
 
-export default connect(mapStateToProps)(OrderConfirmation);
+const mapDispatchToProps = {
+  deleteCart
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderConfirmation);
